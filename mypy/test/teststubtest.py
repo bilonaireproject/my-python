9c6ca04edfd5aedfd53135378722710b16403718
Christoph Tyralla <c.tyralla@bjoernsen.de>
2023-12-29 20:50:06 +0100

    fix stubtest `test_name_mangling`

--- mypy/test/teststubtest.py
+++ mypy/test/teststubtest.py
@@ -1533,7 +1533,7 @@ class StubtestUnit(unittest.TestCase):
                 def __mangle_good(self, text): pass
                 def __mangle_bad(self, text): pass
             """,
-            error="X.__mangle_bad",
+            error="X._X__mangle_bad",
         )
         yield Case(
             stub="""
@@ -1550,7 +1550,7 @@ class StubtestUnit(unittest.TestCase):
                         def __mangle_good(self, text): pass
                         def __mangle_bad(self, text): pass
             """,
-            error="Klass.__Mangled1.__Mangled2.__mangle_bad",
+            error="Klass._Klass__Mangled1._Mangled1__Mangled2._Mangled2__mangle_bad",
         )
         yield Case(
             stub="""
@@ -1563,7 +1563,7 @@ class StubtestUnit(unittest.TestCase):
                 def __mangle_good(self, text): pass
                 def __mangle_bad(self, text): pass
             """,
-            error="__Dunder__.__mangle_bad",
+            error="__Dunder__._Dunder____mangle_bad",
         )
         yield Case(
             stub="""
@@ -1576,7 +1576,7 @@ class StubtestUnit(unittest.TestCase):
                 def __mangle_good(self, text): pass
                 def __mangle_bad(self, text): pass
             """,
-            error="_Private.__mangle_bad",
+            error="_Private._Private__mangle_bad",
         )
 
     @collect_cases
